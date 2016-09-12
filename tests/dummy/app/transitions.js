588e5da7796afcfd9d34b865ae84555b335f11b6
童亮 <tongliang@zhubajie.com>
2016-09-12 15:29:38 +0800

    liquid-tether

--- tests/dummy/app/transitions.js
+++ tests/dummy/app/transitions.js
@@ -1,7 +1,7 @@
 import { target, onOpenTether } from 'zbj-liquid-tether';
 
 const options = {
-  duration: 3000,
+  duration: 300,
   easing: 'easeInOutQuint'
 };
 
@@ -9,87 +9,94 @@ export default function() {
   this.transition(
     target('hello-world'),
     // this.use('tether', ['fade-down', { duration: 400, easing: [600, 22] }])
-    this.use('tether', ['fade-down', { duration: 400 }])
+    this.use('tether', ['fade-up', { duration: 400 }])
   );
 
   /* Modal Examples */
 
   this.transition(
     target('modal-dialog'),
-    this.toValue(({ index: newIndex }, { index: oldIndex }) => newIndex > oldIndex),
-    this.use('tether', ['to-left', options]),
-    this.reverse('tether', ['to-right', options]),
+    this.use('tether', ['fade-down', options]),
+    this.reverse('tether', ['fade-up', options]),
     this.debug()
   );
 
-  this.transition(
-    target('modal-dialog'),
-    this.toValue(({ index }) => index === 1),
-    this.use('tether', 'fade', 'fade')
-  );
+  // this.transition(
+  //   target('modal-dialog'),
+  //   this.toValue(({ index: newIndex }, { index: oldIndex }) => newIndex > oldIndex),
+  //   this.use('tether', ['to-left', options]),
+  //   this.reverse('tether', ['to-right', options]),
+  //   this.debug()
+  // );
 
-  this.transition(
-    target('modal-dialog'),
-    this.toValue(({ index }) => !index),
-    this.use('tether', 'fade', 'fade')
-  );
-
-  /* Flyto Example */
-
-  this.transition(
-    target('flyto-dialog'),
-    this.toValue(({ index }) => !index),
-    this.use('tether', ['to-left', options], 'fade')
-  );
-
-  this.transition(
-    target('flyto-dialog'),
-    this.toValue(({ index: newIndex }, { index: oldIndex }) => newIndex > oldIndex),
-    this.use('tether', ['fly-to', options]),
-    this.reverse('tether', ['fly-to', options])
-  );
-
-  this.transition(
-    target('flyto-dialog'),
-    this.toValue(({ index }) => index === 1),
-    this.use('tether', ['to-right', options], 'fade')
-  );
-
-  /* Flyout Example */
-
-  this.transition(
-    target('flyout'),
-    onOpenTether(),
-    this.use('explode', {
-      pick: '.flyout',
-      use: ['to-left', options]
-    }, {
-      pick: '.modal-backdrop',
-      use: 'fade'
-    }),
-    this.reverse('explode', {
-      pick: '.flyout',
-      use: ['to-right', options]
-    }, {
-      pick: '.modal-backdrop',
-      use: 'fade'
-    })
-  );
-
-  /* Scenarios */
-
-  this.transition(
-    target('component-in-tether'),
-    this.use('tether', ['fade', options])
-  );
-
-  this.transition(
-    target(/multiple-[1,3]/),
-    this.use('tether', ['fade', options])
-  );
-
-  this.transition(
-    target(/multiple-2/),
-    this.use('tether', ['to-up', options])
-  );
+  // this.transition(
+  //   target('modal-dialog'),
+  //   this.toValue(({ index }) => index === 1),
+  //   this.use('tether', 'fade', 'fade')
+  // );
+  //
+  // this.transition(
+  //   target('modal-dialog'),
+  //   this.toValue(({ index }) => !index),
+  //   this.use('tether', 'fade', 'fade')
+  // );
+  //
+  // /* Flyto Example */
+  //
+  // this.transition(
+  //   target('flyto-dialog'),
+  //   this.toValue(({ index }) => !index),
+  //   this.use('tether', ['to-left', options], 'fade')
+  // );
+  //
+  // this.transition(
+  //   target('flyto-dialog'),
+  //   this.toValue(({ index: newIndex }, { index: oldIndex }) => newIndex > oldIndex),
+  //   this.use('tether', ['fly-to', options]),
+  //   this.reverse('tether', ['fly-to', options])
+  // );
+  //
+  // this.transition(
+  //   target('flyto-dialog'),
+  //   this.toValue(({ index }) => index === 1),
+  //   this.use('tether', ['to-right', options], 'fade')
+  // );
+  //
+  // /* Flyout Example */
+  //
+  // this.transition(
+  //   target('flyout'),
+  //   onOpenTether(),
+  //   this.use('explode', {
+  //     pick: '.flyout',
+  //     use: ['to-left', options]
+  //   }, {
+  //     pick: '.modal-backdrop',
+  //     use: 'fade'
+  //   }),
+  //   this.reverse('explode', {
+  //     pick: '.flyout',
+  //     use: ['to-right', options]
+  //   }, {
+  //     pick: '.modal-backdrop',
+  //     use: 'fade'
+  //   })
+  // );
+  //
+  // /* Scenarios */
+  //
+  // this.transition(
+  //   target('component-in-tether'),
+  //   this.use('tether', ['fade', options])
+  // );
+  //
+  // this.transition(
+  //   target(/multiple-[1,3]/),
+  //   this.use('tether', ['fade', options])
+  // );
+  //
+  // this.transition(
+  //   target(/multiple-2/),
+  //   this.use('tether', ['to-up', options])
+  // );
 }
